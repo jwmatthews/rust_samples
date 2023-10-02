500b31325c6c0df8df542b4f128919dd0b6a3bb3
John Matthews <jwmatthews@gmail.com>
2023-10-02 05:22:58 -0400

    Refactored list_users to own function

--- aws/iam/audit_iam/src/lib.rs
+++ aws/iam/audit_iam/src/lib.rs
@@ -1,17 +1,20 @@
 
-use aws_sdk_iam;
+use aws_sdk_iam::{self, Client};
 use aws_sdk_iam::error::SdkError;
-use aws_sdk_iam::operation::list_users::ListUsersError;
+use aws_sdk_iam::operation::list_users::{ListUsersError, ListUsersOutput};
+//use aws_sdk_iam::operation::list_users::_list_users_output::ListUsersOutput;
+use aws_types::sdk_config::SdkConfig;
 
-
-//#[tokio::run]
 pub async fn run() -> Result<(), SdkError<ListUsersError>> {
     // Create a client
-    let shared_config = aws_config::load_from_env().await;
-    let client = aws_sdk_iam::Client::new(&shared_config);
+    let config = aws_config::load_from_env().await;
+    run_with_config(config).await
+}
 
-    // Call the list_users method
-    match client.list_users().send().await {
+pub async fn run_with_config(config: SdkConfig) -> Result<(), SdkError<ListUsersError>> {
+    let client = aws_sdk_iam::Client::new(&config);
+
+    match list_users(client).await {
         Ok(resp) => {
             // Iterate through the users and print their names
             for user in resp.users.unwrap_or_default() {
@@ -24,4 +27,26 @@ pub async fn run() -> Result<(), SdkError<ListUsersError>> {
             Err(e)
         }
     }
+}
+
+
+
+pub async fn list_users(client: Client) -> Result<ListUsersOutput, SdkError<ListUsersError>> {
+    client.list_users().send().await
+
+    // Call the list_users method
+    /*match client.list_users().send().await {
+        Ok(resp) => {
+            // Iterate through the users and print their names
+            for user in resp.users.clone().unwrap_or_default() {
+                println!("{}", user.user_name.unwrap_or_default());
+            }
+            Ok(resp)
+        }
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            Err(e)
+        }
+    }
+     */
 }
